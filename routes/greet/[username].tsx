88393292d19e5613ea65270c845d3cda74da3cc8
Gian Lazaro <hello@gianlazaro.com>
2023-08-03 06:36:11 -0700

    adjust url params

--- routes/greet/[username].tsx
+++ routes/greet/[username].tsx
@@ -2,7 +2,7 @@ import { RouteContext } from "$fresh/server.ts";
 
 export default async function Greet(_req: Request, ctx: RouteContext) {
   const { username } = ctx.params;
-  const resp = await fetch(`https://api.github.com/users/gianlazaro`);
+  const resp = await fetch(`https://api.github.com/users/${username}`);
   const user = await resp.json();
   if (!user) {
     return <h1>No data</h1>;
